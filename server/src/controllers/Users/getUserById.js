d4dcb49e3b4c4cb176f9c861ca11c86fd36116e4
Esteban Yguia <estebanyguia16@gmail.com>
2023-10-26 15:36:25 -0500

    limpieza de codigo userbyid

--- server/src/controllers/Users/getUserById.js
+++ server/src/controllers/Users/getUserById.js
@@ -2,7 +2,6 @@ const db = require("../../db");
 
 const getUser = async (clientId) => {
   try {
-    //const user = await db.User.findOne({ where: { clientId } });
     const user = await db.User.findByPk(clientId);
     if (!user) {
       throw new Error("User not found");
