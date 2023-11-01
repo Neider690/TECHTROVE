cd6d65b23ec40bfdb7e85a290f3900eefb66952d
AveMat81 <matiasavesani@yahoo.com.ar>
2023-11-01 19:20:06 -0300

    cambio en ruta user y singUp

--- server/src/controllers/Users/singUp.js
+++ server/src/controllers/Users/singUp.js
@@ -1,17 +1,32 @@
 const db = require("../../db");
 const { sendRegistrationEmail } = require("../../utils/email");
 
-const signUp = async (clientId, name, email, imageProfile) => {
-  const newUser = await db.User.create({
-    clientId,
-    name,
-    email,
-    image:{url: imageProfile.url, public_id: imageProfile.public_id},
+const signUp = async (user) => {
+  // const newUser = await db.User.create({
+  //   // clientId,
+  //   // name,
+  //   // email,
+  //   // image:{url: imageProfile.url, public_id: imageProfile.public_id},
+  //     name: user.name,
+  //     username: user.nickname,
+  //     email: user.email,
+  //     image: user.picture 
+     
+  // });
+
+  const [userFound, userCreated] = await db.User.findOrCreate({
+    where: { email: user.email, },
+    defaults: {
+      name: user.name,
+      username: user.nickname,
+      email: user.email,
+      image: user.picture 
+    }
   });
 
-  sendRegistrationEmail(newUser.clientId);
+  //sendRegistrationEmail(newUser.clientId);
 
-  return newUser;
+  return (userFound ? userFound : userCreated);
 };
 
 module.exports = signUp;
